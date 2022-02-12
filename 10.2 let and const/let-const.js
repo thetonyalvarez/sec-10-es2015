5f7d6f2bd2da16a3aaa8aa3bd061c5e112f21455
Tony Alvarez <tonyalva87@gmail.com>
2022-02-11 19:56:27 -0800

    completed arrow functions exercise

--- 10.2 let and const/let-const.js
+++ 10.2 let and const/let-const.js
@@ -1,11 +1,11 @@
 // ES5 Global Constants
-var PI = 3.14;
-PI = 42; // stop me from doing this!
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
 
 // ES2015 Global Constants
 /* Write an ES2015 Version */
 const PI = 3.14;
-PI = 42;
+PI = 42; // should return error
 // PI should be a constant, so we should use const to prevent the variable from being changed.
 
 // Quiz
---
+++ 10.3 Arrow Functions/arrow-functions.js
@@ -0,0 +1,25 @@
+// ES5 Map Callback
+// function double(arr) {
+// 	return arr.map(function (val) {
+// 		return val * 2;
+// 	});
+// }
+
+// ES2015 Arrow Functions Shorthand
+// Refactor the above code to use two arrow functions. Turn it into a one-liner.
+/* Write an ES2015 Version */
+const double = (arr) => arr.map((val) => val * 2);
+
+// Refactor the following function to use arrow functions:
+// Replace ALL functions with arrow functions:
+// const squareAndFindEvens = (numbers) => {
+// 	var squares = numbers.map((num) => {
+// 		return num ** 2;
+// 	});
+// 	var evens = squares.filter((square) => {
+// 		return square % 2 === 0;
+// 	});
+// 	return evens;
+// };
+
+const squareAndFindEvens = (numbers) =>	numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
