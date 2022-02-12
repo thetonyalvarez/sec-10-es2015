41e9094757d0259e07c89f45199c67586a2e472e
Tony Alvarez <tonyalva87@gmail.com>
2022-02-12 11:30:35 -0800

    created one test.html file to test all .js files in this project. also changed let-const.js into an .html to make it more readable, since I am not actually running any test exercises in the 10.2 subunit.

---
+++ 10.3 Arrow Functions/arrow-functions.test.js
@@ -0,0 +1,14 @@
+describe("Arrow Functions", () => {
+	it("should double all values in the array", () => {
+		const arr = [1, 2, 3, 4];
+		const doubledArr = [2, 4, 6, 8];
+		expect(double(arr)).toEqual(doubledArr);
+	});
+
+	it("should return only even numbers from the returned array using squareAndFindEvens()", () => {
+		const arr = [1, 2, 3, 4];
+		const squaredArr = [1, 4, 9, 16];
+		const evens = [4, 16];
+		expect(squareAndFindEvens(arr)).toEqual(evens);
+	});
+});
